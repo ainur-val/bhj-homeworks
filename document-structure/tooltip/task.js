e9f2cd2500d7e556c49800c171fa54051ad0de32
Aynur Valiev <ainur-val@mail.ru>
2025-05-21 23:22:14 +0500

    Исправил замечания

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -3,13 +3,18 @@ hasTooltip[0].insertAdjacentHTML("afterEnd", "<div class='tooltip' style='left: 
 const tooltip = document.querySelector(".tooltip");
 
 for (let i = 0; i < hasTooltip.length; i++) {
-  hasTooltip[i].addEventListener("mousedown", (event) => {
-    event.preventDefault();
-    tooltip.classList.add("tooltip_active");
-        let text = hasTooltip[i].getAttribute("title");
-        tooltip.innerText = text;
-        tooltip.style.top = `${(hasTooltip[i].getBoundingClientRect().top + 20)}px`;
-        tooltip.style.left = `${hasTooltip[i].getBoundingClientRect().left}px`;
-        
-  });
+	hasTooltip[i].addEventListener("click", (event) => {
+		event.preventDefault();
+		let text = hasTooltip[i].getAttribute("title");
+
+		if (tooltip.innerText == text) {
+			tooltip.classList.toggle("tooltip_active");
+		} else {
+			tooltip.classList.add("tooltip_active");
+			tooltip.innerText = text;
+			tooltip.style.top = `${(hasTooltip[i].getBoundingClientRect().top + 20)}px`;
+			tooltip.style.left = `${hasTooltip[i].getBoundingClientRect().left}px`;
+		}
+	});
+
 }
