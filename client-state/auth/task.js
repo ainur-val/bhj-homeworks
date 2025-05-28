573caa96dc803118fcfa7927a964a54edf12a850
Aynur Valiev <ainur-val@mail.ru>
2025-05-28 21:56:01 +0500

    Внес изменения в код в соответствии с замечаниями

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -3,24 +3,34 @@ const signinForm = document.getElementById("signin__form");
 const form = document.forms.signin__form;
 const xhr = new XMLHttpRequest();
 
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('id') !== null) {
+		document.querySelector('.signin').classList.remove('signin_active');
+		document.querySelector('.welcome').classList.add('welcome_active');
+				document.getElementById('user_id').textContent = localStorage.getItem('id');
+    }
+  });
+
 signinForm.addEventListener("submit", (e) => {
 	e.preventDefault();
-	xhr.addEventListener('readystatechange', () => {
-		if (xhr.readyState === xhr.DONE) {
-			const response = JSON.parse(xhr.responseText);
-			if (response['success']) {
+
+	xhr.addEventListener('load', () => {
+		const response = xhr.response;
+		if (response['success']) {
 				document.querySelector('.signin').classList.remove('signin_active');
 				document.querySelector('.welcome').classList.add('welcome_active');
 
 				localStorage.setItem('id', response['user_id']);
 				document.getElementById('user_id').textContent = response['user_id'];
-			} else {
+		} else {
 				alert('Неверный логин/пароль');
-			}
 		}
+		
+		signinForm.reset();
 	});
 
 	xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
 	const formData = new FormData(form);
+	xhr.responseType = 'json';
 	xhr.send(formData);
 })
